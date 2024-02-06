07e9c4b1adfc5d1586f34965f0fe456c8ce4f439
Amit Kumar Pradhan <akamitpradhan@gmail.com>
2024-02-06 07:42:56 +0530

    Update StartApplication.java

--- src/main/java/com/example/demo/StartApplication.java
+++ src/main/java/com/example/demo/StartApplication.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@Controller
+// @Controller
 public class StartApplication {
 
     @GetMapping("/")
