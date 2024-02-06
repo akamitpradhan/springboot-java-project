d0671be017c3290bb5791ae648726d1dd713e63c
Amit Kumar Pradhan <akamitpradhan@gmail.com>
2024-02-06 07:38:15 +0530

    Update StartApplication.java

--- src/main/java/com/example/demo/StartApplication.java
+++ src/main/java/com/example/demo/StartApplication.java
@@ -7,12 +7,19 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@SpringBootApplication
+@Controller
 public class StartApplication {
 
-	@GetMapping("/")
-	public String index() {
-		return "Greetings from Spring Boot!";
-	}
+    @GetMapping("/")
+    public String index(final Model model) {
+        model.addAttribute("title", "This is a SpringBoot Static Web Application");
+        model.addAttribute("msg", "Application Is Deployed To Kuberneets");
+        return "index";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(StartApplication.class, args);
+    }
 
 }
