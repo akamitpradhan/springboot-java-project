a7f8f149dd11d4d203655655761db4bd78bd678e
Amit Kumar Pradhan <akamitpradhan@gmail.com>
2024-02-06 07:35:38 +0530

    Update StartApplication.java

--- src/main/java/com/example/demo/StartApplication.java
+++ src/main/java/com/example/demo/StartApplication.java
@@ -1,9 +1,9 @@
 package com.example.demo;
 
-// import org.springframework.boot.SpringApplication;
-// import org.springframework.boot.autoconfigure.SpringBootApplication;
-// import org.springframework.stereotype.Controller;
-// import org.springframework.ui.Model;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
